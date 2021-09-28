e128c314107191a1234cc3b99d3f43aaf1a0e23e
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2021-09-28 16:00:14 +1000

    Updated controler to be compatible with our pin configuration and to support all pins we're using

--- controler/controler.ino
+++ controler/controler.ino
@@ -1,41 +1,42 @@
-#define Pin1 31
-#define Pin2 33
-#define Pin3 35
+const int PIN_1 = 35, PIN_2 = 37, PIN_3 = 39, PIN_4 = 41;
+const char EMS_ON = HIGH, EMS_OFF = LOW;
 
-char data = 0;                //Variable for storing received data
+char data = 0;  // Variable for storing received data
 
-char EMSOn = HIGH;
-char EMSOff = LOW;
-
-
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  pinMode(Pin1, OUTPUT);
-  pinMode(Pin2, OUTPUT);
-  pinMode(Pin3, OUTPUT);
-  digitalWrite(Pin1, EMSOff);
-  digitalWrite(Pin2, EMSOff);
-  digitalWrite(Pin3, EMSOff);
+  pinMode(PIN_1, OUTPUT);
+  pinMode(PIN_2, OUTPUT);
+  pinMode(PIN_3, OUTPUT);
+  pinMode(PIN_4, OUTPUT);
+  digitalWrite(PIN_1, EMS_OFF);
+  digitalWrite(PIN_2, EMS_OFF);
+  digitalWrite(PIN_3, EMS_OFF);
+  digitalWrite(PIN_4, EMS_OFF);
 }
-void loop()
-{
-  if (Serial.available() > 0) // Send data only when you receive data:
+
+void loop() {
+  if (Serial.available() > 0)  // Send data only when you receive data:
   {
-    data = Serial.read();      //Read the incoming data and store it into variable data
+    // Read the incoming data and store it into variable data
+    data = Serial.read();
     Serial.print(data);
     Serial.print("\n");
     if (data == 'A')
-      digitalWrite(Pin1, EMSOn);
+      digitalWrite(PIN_1, EMS_ON);
     else if (data == 'a')
-      digitalWrite(Pin1, EMSOff);
+      digitalWrite(PIN_1, EMS_OFF);
     else if (data == 'B')
-      digitalWrite(Pin2, EMSOn);
+      digitalWrite(PIN_2, EMS_ON);
     else if (data == 'b')
-      digitalWrite(Pin2, EMSOff);
+      digitalWrite(PIN_2, EMS_OFF);
     else if (data == 'C')
-      digitalWrite(Pin3, EMSOn);
+      digitalWrite(PIN_3, EMS_ON);
     else if (data == 'c')
-      digitalWrite(Pin3, EMSOff);
+      digitalWrite(PIN_3, EMS_OFF);
+    else if (data == 'D')
+      digitalWrite(PIN_4, EMS_ON);
+    else if (data == 'd')
+      digitalWrite(PIN_4, EMS_OFF);
   }
 }
