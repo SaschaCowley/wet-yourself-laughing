cf85cfb615226c60799f10b185d69d04204918c7
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2021-10-10 11:59:08 +1100

    New enums

--- wysl/wysl/enums.py
+++ wysl/wysl/enums.py
@@ -7,7 +7,13 @@ class StatusEnum(Enum):
     """Enumeration of statuses."""
 
     CAMERA_ERROR = auto()
+    MICROPHONE_ERROR = auto()
+    NO_LAUGHTER_DETECTED = auto()
     LAUGHTER_DETECTED = auto()
+    NO_SMILE_DETECTED = auto()
+    LOW_INTENSITY_SMILE_DETECTED = auto()
+    MEDIUM_INTENSITY_SMILE_DETECTED = auto()
+    HIGH_INTENSITY_SMILE_DETECTED = auto()
 
 
 class CommandEnum(Enum):
