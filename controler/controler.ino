fd997d080535230d031c5bd983955691e8157da0
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2021-10-12 09:57:06 +1100

    Newer, more sophisticated controler code that supports pulsing and querying relays.

--- controler/controler.ino
+++ controler/controler.ino
@@ -1,42 +1,110 @@
-const int PIN_1 = 35, PIN_2 = 37, PIN_3 = 39, PIN_4 = 41;
+/* Code for the Arduino EMS controller.
+ *
+ * This code enables the relays on the Arduino to be controlled and queried via
+ * serial to allow digital switching of EMS from other programs.
+ *
+ * Supported commands:
+ * Command                    Syntax              Return
+ * Switch relay on            +<relay>            <Nothing>
+ * Switch relay off           -<relay>            <Nothing>
+ * Periodically pulse relay   !<relay><interval>  <Nothing>
+ * Query relay state          ?<relay>            0 if off; 1 if on
+ *
+ * <relay>    must be one of 'A', 'B', 'C' or 'D' for relays 1-4, respectively.
+ *            Currently, relay 1 maps to pin 35, 2 to 37, 3 to 39 and 4 to 41.
+ * <interval> Must be an integer number of milliseconds to wait between
+ *            switching the state of the given relay. The interval between
+ *            successive switches should always be equal to or exceed this
+ *            interval. Set to 0 to disable pulsing the relay. Note that the
+ *            state of the relay after pulsing is stopped is not guaranteed.
+ * Note:      All commands silently ignore invalid input while still consuming
+ *            two bytes of data from the serial connection. However, this is
+ *            reset every 100ms.
+ */
+
+const int PIN[4] = {35, 37, 39, 41};
 const char EMS_ON = HIGH, EMS_OFF = LOW;
 
-char data = 0;  // Variable for storing received data
+unsigned long last_pulse[4] = {0, 0, 0, 0};
+long pulse_interval[4] = {0, 0, 0, 0};
 
 void setup() {
+  /* Setup code. */
   Serial.begin(9600);
-  pinMode(PIN_1, OUTPUT);
-  pinMode(PIN_2, OUTPUT);
-  pinMode(PIN_3, OUTPUT);
-  pinMode(PIN_4, OUTPUT);
-  digitalWrite(PIN_1, EMS_OFF);
-  digitalWrite(PIN_2, EMS_OFF);
-  digitalWrite(PIN_3, EMS_OFF);
-  digitalWrite(PIN_4, EMS_OFF);
+  // Reduce serial read timeout or it causes too much of a delay.
+  Serial.setTimeout(100);
+
+  // Configure pins to be in output mode and set them to a known value.
+  for (int i = 0; i < 4; i++) {
+    pinMode(PIN[i], OUTPUT);
+    digitalWrite(PIN[i], EMS_OFF);
+  }
 }
 
 void loop() {
-  if (Serial.available() > 0)  // Send data only when you receive data:
-  {
-    // Read the incoming data and store it into variable data
-    data = Serial.read();
-    Serial.print(data);
-    Serial.print("\n");
-    if (data == 'A')
-      digitalWrite(PIN_1, EMS_ON);
-    else if (data == 'a')
-      digitalWrite(PIN_1, EMS_OFF);
-    else if (data == 'B')
-      digitalWrite(PIN_2, EMS_ON);
-    else if (data == 'b')
-      digitalWrite(PIN_2, EMS_OFF);
-    else if (data == 'C')
-      digitalWrite(PIN_3, EMS_ON);
-    else if (data == 'c')
-      digitalWrite(PIN_3, EMS_OFF);
-    else if (data == 'D')
-      digitalWrite(PIN_4, EMS_ON);
-    else if (data == 'd')
-      digitalWrite(PIN_4, EMS_OFF);
+  /* Handle pulsing of relays.
+   * Reading of commands is done in serialEvent().
+   */
+  for (int i = 0; i < 4; i++) {
+    if (pulse_interval[i] > 0) {
+      if (millis() - last_pulse[i] >= pulse_interval[i]) {
+        if (digitalRead(PIN[i]) == LOW)
+          digitalWrite(PIN[i], HIGH);
+        else
+          digitalWrite(PIN[i], LOW);
+        last_pulse[i] = millis();
+      }
+    }
+  }
+}
+
+void serialEvent() {
+  /* Read commands over serial. */
+  char cmdstr[2];
+  int pinNo;
+
+  // Commands are always at least two characters long.
+  Serial.readBytes(cmdstr, 2);
+
+  // Get the index of the pin to which the command refers.
+  switch (cmdstr[1]) {
+    case 'A':
+      pinNo = 0;
+      break;
+    case 'B':
+      pinNo = 1;
+      break;
+    case 'C':
+      pinNo = 2;
+      break;
+    case 'D':
+      pinNo = 3;
+      break;
+    default:
+      // Erroneous pin.
+      return;
+  }
+
+  // Act on the command.
+  switch (cmdstr[0]) {
+    case '+':
+      // Allow EMS through relay.
+      digitalWrite(PIN[pinNo], EMS_ON);
+      break;
+    case '-':
+      // Block EMS through relay.
+      digitalWrite(PIN[pinNo], EMS_OFF);
+      break;
+    case '!':
+      // Pulse relay.
+      pulse_interval[pinNo] = Serial.parseInt();
+      break;
+    case '?':
+      // Query state of relay.
+      Serial.println(digitalRead(PIN[pinNo]));
+      break;
+    default:
+      // Erroneous command.
+      return;
   }
 }
