575d36417b04460d61ec4e47e172946306992012
Sascha Cowley <16543535+SaschaCowley@users.noreply.github.com>
2021-10-11 13:07:02 +1100

    Modified to use selectors

--- spikes/client.py
+++ spikes/client.py
@@ -1,16 +1,27 @@
 import socket
+import selectors
 
 address = ("127.0.0.1", 5005)
 
-soc = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-soc.bind(address)
-print(f'Receiving from {address[0]}:{address[1]}')
+sel = selectors.DefaultSelector()
 
+def recv(sock, mask):
+    data, addr = sock.recvfrom(1024)
+    print(f'Received {data} from {addr}')
+
+sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+sock.bind(address)
+sock.setblocking(False)
+sel.register(sock, selectors.EVENT_READ, recv)
+
+print(f'Receiving from {address[0]}:{address[1]}')
 while True:
     try:
-        data, addr = soc.recvfrom(1024)
-        print(f'Received {data}\n{addr}\n\n')
+        events = sel.select(0)
+        for key, mask in events:
+            callback = key.data
+            callback(key.fileobj, mask)
     except KeyboardInterrupt:
         break
 
-soc.close()
+sock.close()
